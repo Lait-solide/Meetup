f02989e2d0773bee40d0c699c5ab2b18ccc8c5b4
Ahmed <ahmed.78.nasser@gmail.com>
2024-09-09 22:39:02 +0200

    Event list item, styles with nativewind, custom component, props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
+import Feather from '@expo/vector-icons/Feather';
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
+import EventListItem from '~/components/EventListItem';
+import events from '~/assets/events.json';
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
+
+      {/* Event list item */}
+      <EventListItem event={events[0]} />
+      <EventListItem event={events[1]} />
+      <EventListItem event={events[2]} />
+      <EventListItem event={events[3]} />
     </>
   );
 }
